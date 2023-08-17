9e9e3d8b0a6723c72c7472a7f9f4c2cb64dfee59
inzamamul <inzamamul.rifat@gmail.com>
2023-08-17 20:58:07 +0600

    isi purchase report

--- resources/views/bof-reports/isi/isi-purchase-report.blade.php
+++ resources/views/bof-reports/isi/isi-purchase-report.blade.php
@@ -8,113 +8,120 @@
     <link href="{{ asset('style/report-style.css') }}" rel="stylesheet">
 
     <style>
-    @page {
-        margin-top: 5px;
-    }
+        @page {
+            margin-top: 5px;
+        }
 
-    .borderTable{
-		border: 1px solid;
-		border-collapse: collapse;
-		text-align: center;
-    } 
+        .borderTable {
+            border: 1px solid;
+            border-collapse: collapse;
+            text-align: center;
+        }
 
-    table.center {
-        margin-left: auto;
-        margin-right: auto;
-        width: 100%;
-        padding: 5px;
-    }
+        table.center {
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+            padding: 5px;
+        }
     </style>
 
 </head>
 
 
 <body>
-    <?php
-    $en = [];
-    $bn = [];
-    $data = json_decode($val['data']);
-    ?>
+<?php
+$en = [];
+$bn = [];
+$data = json_decode($val['data']);
+?>
 
-    <div style="font-size: 24px; ">
-        <table width="100%">
+<div style="font-size: 24px; ">
+    <table width="100%">
+        <tr>
+            <td style="width: 33%"></td>
+            <td style="width: 34%; text-align: center;">
+                সীমিত
+                <br>
+                <u>বাংলাদেশ সমরাস্ত্র কারখানা (বি ও এফ)</u>
+                <br>
+                (আই এস আই শাখা)
+
+            </td>
+            <td style="width: 33%; text-align: center;">
+                <!-- <br>
+            বিওএফ নম্বরঃ -->
+            </td>
+        </tr>
+    </table>
+    <table width="100%">
+        <tr>
+            <td>এল এম <br>
+                {{$Controller::enToBnConveter($data->master ? $data->master->lmNo : '')}}
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 100% !important; text-align: left;">
+                <u><span style="font-size: 16px;">{{($data->master ? $data->master->subject : '')}} </span></u>
+            </td>
+        </tr>
+        <tr>
+            <td>{!! (optional($data->master)->reference) !!}</td>
+        </tr>
+        <tr>
+            <td style="width: 100% !important; text-align: left;">
+
+            </td>
+        </tr>
+    </table>
+    <br>
+    <div style="text-align: center;">
+
+        <table class="center tbBorder" style="text-align: center; padding: 5px;">
+            <thead>
             <tr>
-                <td style="width: 33%"></td>
-                <td style="width: 34%; text-align: center;">
-                    সীমিত
-                    <br>
-                    <u>বাংলাদেশ সমরাস্ত্র কারখানা (বি ও এফ)</u>
-                    <br>
-						(আই এস আই শাখা)
-
-                </td>
-                <td style="width: 33%; text-align: center;">
-                    <!-- <br>
-                বিওএফ নম্বরঃ -->
-                </td>
+                <th style="width: 10%; padding: 2px; ">ক্রঃ</th>
+                <th style="width: 35%; padding: 2px; ">দ্রব্যের নাম</th>
+                <th style="width: 20%; padding: 2px; ">পরিমাণ</th>
+                <th style="width: 35%; padding: 2px; ">দ্রব্যের বিবরণ</th>
             </tr>
-        </table>
-		 <table width="100%">
-		 <tr>
-			<td>এল এম  <br>
-				{{$Controller::enToBnConveter($data->master ? $data->master->lmNo : '')}}
-			</td>
-		 </tr>
-		 <tr>
-			<td style="width: 100% !important; text-align: left;">
-				<u><span style="font-size: 16px;">{{($data->master ? $data->master->subject : '')}} </span></u>
-			</td>
-		 </tr>
-		 <tr>
-             <td>{!! (optional($data->master)->reference) !!}</td>
-          </tr>
-		  <tr>
-			<td style="width: 100% !important; text-align: left;">
-				
-			</td>
-		 </tr>
-		 </table>
-		 <br>
-		 <div style="text-align: center;">
-		 
-		 <table class="center tbBorder" style="text-align: center; padding: 5px;">
-                <thead>
-                    <tr>
-                        <th style="width: 10%; padding: 2px; ">ক্রঃ</th>
-                        <th style="width: 70%; padding: 2px; ">দ্রব্যের বিবরণ </th>
-                        <th style="width: 20%; padding: 2px; ">পরিমাণ </th>
-                    </tr>
-                </thead>
-				<tbody>
-                    @foreach ($data->details as $index => $list)
-                    <tr>
-                        <td style="width: 10%; padding: 2px; ">{{$Controller::enToBnConveter($index+1,'number')}} |</td>
-						<td>
-						@if($list->sampleRegisterDetails != null && $list->sampleRegisterDetails->itemName != null)
+            </thead>
+
+            <tbody>
+            @foreach ($data->details as $index => $list)
+                <tr>
+                    <td style="width: 10%; padding: 2px; ">{{$Controller::enToBnConveter($index+1,'number')}} |</td>
+                    <td>
+                        @if($list->sampleRegisterDetails != null && $list->sampleRegisterDetails->itemName != null)
                             <span>{{optional($list->sampleRegisterDetails)->itemName}}</span>
-                            @endif
-						</td>
-						<td>
-						@if($list->sampleRegisterDetails != null && $list->sampleRegisterDetails->receiveQty != null)
+                        @endif
+                    </td>
+                    <td>
+                        @if($list->sampleRegisterDetails != null && $list->sampleRegisterDetails->receiveQty != null)
                             <span>
 							{{$Controller::enToBnConveter($list->sampleRegisterDetails->receiveQty)}} পিছ </span>
-                            @endif</td>
-						<td></td>
-						<td></td>
-					</tr>
-					@endforeach
-		 </table>
-		 <div>
-		 <br>
-		 <table width="100%">
-			<tr>
-				<td>{!! (optional($data->master)->body) !!}</td>
-			</tr>
-		</table>
-		
-		<br>
-		
-		<table width="100%">
+                        @endif
+                    </td>
+                    <td>
+                        @if($list->sampleRegisterDetails != null && $list->sampleRegisterDetails->itemSpecification != null)
+                            <span>{{optional($list->sampleRegisterDetails)->itemSpecification}}</span>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <div>
+            <br>
+            <table width="100%">
+                <tr>
+                    <td>{!! (optional($data->master)->body) !!}</td>
+                </tr>
+            </table>
+
+            <br>
+
+            <table width="100%">
                 <tr>
                     <td width="50%"></td>
                     <td width="20%"></td>
@@ -122,7 +129,7 @@
                         <div style="float: right;">
                             @if($data->master->manager != null &&
                             $data->master->manager->employeeNameBangla != null)
-                            <span>{{ optional($data->master->manager)->employeeNameBangla }}</span>
+                                <span>{{ optional($data->master->manager)->employeeNameBangla }}</span>
                             @endif
                             <br>
 
@@ -131,9 +138,9 @@
                             null && $data->master->manager->employeeOfficialInformation->designation != null
                             && $data->master->manager->employeeOfficialInformation->designation->banglaName != null
                             )
-                            <span>{{ optional($data->master->manager->employeeOfficialInformation->designation)->banglaName }}</span>
+                                <span>{{ optional($data->master->manager->employeeOfficialInformation->designation)->banglaName }}</span>
                             @endif
-                            
+
 
                         </div>
                     </td>
@@ -141,19 +148,21 @@
 
             </table>
 
-		<table width="100%">
+            <table width="100%">
                 <tr>
                     <td>{!! (optional($data->master)->footer) !!}</td>
                 </tr>
 
             </table>
 
+        </div>
+
+        <footer style="position: fixed; bottom: 0; width: 100%; font-size: 20px; text-align: center">
+            <p>
+                সীমিত
+            </p>
+        </footer>
+
     </div>
-
-    <footer style="position: fixed; bottom: 0; width: 100%; font-size: 20px; text-align: center">
-        <p>
-            সীমিত
-        </p>
-    </footer>
-
+</div>
 </body>
