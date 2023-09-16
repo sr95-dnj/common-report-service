aab663cee68b5cf1f69e268d27bd82a3ccd95fe9
sr95 <sr95.dnj@gmail.com>
2023-09-16 21:55:03 +0600

    p 3

--- resources/views/bof-reports/purchase/tender-price-annex-b.blade.php
+++ resources/views/bof-reports/purchase/tender-price-annex-b.blade.php
@@ -37,18 +37,53 @@ $bn = [];
 $data = json_decode($val['data']);
 ?>
 
-<div style="font-size: 24px; ">
+<div>
     <table width="100%">
         <tr>
-            <td style="width: 25%"></td>
-            <td style="width: 50%; text-align: center;">
-                BANGLADESH ORDNANCE FACTORY (BOF) <br>
-                GAZIPUR CANTONMENT, GAZIPUR-1703
-                web: www.bof.gov.bd, email: <br>
-                <u>man.pur.admin@bof.gov.bd</u>
+            <td style="width: 33%"></td>
+            <td style="width: 34%; text-align: center; vertical-align: top;">
+                <span style="font-size: 10px;">RESTRICTED</span>
             </td>
-            <td style="width: 25%; text-align: center;">
+            <td style="width: 33%; text-align: left;">
+                <span style="font-size: 10px;">
+                    ANNEX-B TO <br>
+                TENDER NO: <span></span><br>
+                DATED: <span></span>
+                </span>
             </td>
+        </tr>
+        <tr>
+            <td colspan="3" style="width: 100%; text-align: center;">
+                <span style="font-size: 10px;">
+                    <u>
+                        BANGLADESH ORDNANCE FACTORIES (BOF) GAZIPUR CANTONMENT, GAZIPUR <br>
+                        TENDER PRICE SCHEDULE
+                        </u>
+                </span>
+            </td>
+        </tr>
+    </table>
+
+    <table width="100%">
+        <tr>
+            <td style="width: 45%; font-size: 11px;">Tender No...................................................... Dated.............</td>
+            <td style="width: 55%; font-size: 11px;">Principal’s Proforma Invoice/ proposal No:................... Dated................</td>
+        </tr>
+        <tr>
+            <td style="width: 45%; font-size: 11px;">Name of Item................................................... Qty...............</td>
+            <td style="width: 55%; font-size: 11px;">Local Agent’s Ref. No: ........................................................... Dated................</td>
+        </tr>
+        <tr>
+            <td style="width: 45%; font-size: 11px;">Name of the local Agent : ...................................................</td>
+            <td style="width: 55%; font-size: 11px;"></td>
+        </tr>
+        <tr>
+            <td style="width: 45%; font-size: 11px;">Name of Manufacturer and Address : ............................</td>
+            <td style="width: 55%; font-size: 11px;"></td>
+        </tr>
+        <tr>
+            <td style="width: 45%; font-size: 11px;">Name of Principal and Address : ......................................</td>
+            <td style="width: 55%; font-size: 11px;"></td>
         </tr>
     </table>
 
@@ -60,124 +95,128 @@ $data = json_decode($val['data']);
     <div style="text-align: center;">
         <table class="center tbBorder" style="text-align: center; padding: 5px;">
             <thead>
-            <tr>
-                <td style="width: 10%; padding: 2px; ">ক্রমিক</td>
-                <td style="width: 40%; padding: 2px; ">বিষয়</td>
-                <td style="width: 50%; padding: 2px; ">বিবরণ</td>
+            <tr style="font-size: 10px;">
+                <td style="width: 5%; padding: 2px; vertical-align: top;">Model No</td>
+                <td style="width: 5%; padding: 2px; vertical-align: top;">Qty</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Total FOB price of Main Equipment</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Total Standard Accessories & Attachment</td>
+                <td style="width: 5%; padding: 2px; vertical-align: top;">Total FOB</td>
+                <td style="width: 5%; padding: 2px; vertical-align: top;">Total Freight</td>
+                <td style="width: 5%; padding: 2px; vertical-align: top;">Total CFR Price</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Fast Moving Spare Parts for Years</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Slow Moving Spare Parts for Years</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Test Trial Run and Training for ...... working Days at BOF Site</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Foreign Training <br>......... working days of BOF Personnel</td>
+                <td style="width: 10%; padding: 2px; vertical-align: top;">Total Price (item)</td>
+                <td style="width: 5%; padding: 2px; vertical-align: top;">Optional item (If any)</td>
             </tr>
 
             </thead>
 
             <tbody>
-            <tr>
-                <td>১</td>
-
-                <td style="text-align: center;">
-                    <span>চাহিদাপত্র নম্বর ও তারিখ</span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
-
-
-            <tr>
-                <td>২</td>
-
-                <td style="text-align: center;">
-                    <span>চাহিদাপত্রে এসিসিডিএফ(বিওএফ ) কর্তৃক আর্থিক স্বীকৃতি </span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
-
-
-            <tr>
-                <td>৩</td>
-
-                <td style="text-align: center;">
-                    <span>চাহিদাপত্রে পণ্যের নাম , পরিমান ও সংখ্যা </span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
-
-
-            <tr>
-                <td>৪</td>
-
-                <td style="text-align: center;">
-                    <span>চাহিদাপত্র প্রাপ্তির  তারিখ </span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
-
-
-            <tr>
-                <td>৫</td>
-
-                <td style="text-align: center;">
-                    <span>দরপত্র নম্বর ও তারিখ </span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
-
-            <tr>
-                <td>৬</td>
-
-                <td style="text-align: center;">
-                    <span>বিওএফ এর ওয়েব সাইডে বিজ্ঞপ্তি প্রকাশের জন্য উপস্থাপন </span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
-
-            <tr>
-                <td> ৭</td>
-
-                <td style="text-align: center;">
-                    <span>মন্তব্য </span>
-                </td>
-
-                <td style="text-align: center;">
-                </td>
-            </tr>
+                <tr>
+                    <td class="text-center">(a)</td>
+                    <td class="text-center">(b)</td>
+                    <td class="text-center">(c)</td>
+                    <td class="text-center">(d)</td>
+                    <td class="text-center">(e)</td>
+                    <td class="text-center">(f)</td>
+                    <td class="text-center">(g)</td>
+                    <td class="text-center">(h)</td>
+                    <td class="text-center">(i)</td>
+                    <td class="text-center">(j)</td>
+                    <td class="text-center">(k)</td>
+                    <td class="text-center">(l)</td>
+                    <td class="text-center">(m)</td>
+                </tr>
+                <tr>
+                    <td class="text-center">&nbsp;</td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                </tr>
             </tbody>
+        </table>
+
+        <table width="100%">
+            <tr>
+                <td colspan="4">
+                    <b style="font-size: 11px"><u><b>N.B</b></u>. &nbsp;&nbsp;  All quoted price mentioned in separately.</b>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 5%; font-size: 11px;">a.</td>
+                <td style="width: 45%; font-size: 11px;">Local Agent's Commission :.........................</td>
+                <td style="width: 5%; font-size: 11px;">b.</td>
+                <td style="width: 45%; font-size: 11px;">Validity of the Offer :.............................</td>
+            </tr>
+            <tr>
+                <td style="width: 5%; font-size: 11px;">c.</td>
+                <td style="width: 45%; font-size: 11px;">Delivery :............................................................</td>
+                <td style="width: 5%; font-size: 11px;">d.</td>
+                <td style="width: 45%; font-size: 11px;">Country of Origin :..................................</td>
+            </tr>
+            <tr>
+                <td style="width: 5%; font-size: 11px;">e.</td>
+                <td style="width: 45%; font-size: 11px;">Warranty :.........................................................</td>
+                <td style="width: 5%; font-size: 11px;">f.</td>
+                <td style="width: 45%; font-size: 11px;">Payment Terms :......................................</td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;"><u><b>Remarks</b></u>:</td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;">
+                    1. &nbsp;&nbsp;&nbsp;&nbsp; Technical & Financial offer should be submitted ‘in <b>Original</b>’in <b>Principals official pad</b> duly signed and sealed by a legal executive of the
+                    principal / company. 03 Sets (<b>01 set x original and 02 set x copy</b>) of offers (<b>For both Technical and Financial Offer</b>) to be submitted in double
+                    Envelop separately.
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;">
+                    2. &nbsp;&nbsp;&nbsp;&nbsp; In Technical and Financial Offer Foreign Principal should confirm regarding 80% and 20% payment system.
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;">
+                    3. &nbsp;&nbsp;&nbsp;&nbsp; If the supplied stores do not function properly, the stores has to be replaced by the supplier at their own expenses within 90 days. If the stores
+                    are not replaced the supplier will be liable to return the paid 80% FOB/CFR/LC value (As applicable).
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;">
+                    4. &nbsp;&nbsp;&nbsp;&nbsp; All above information must be in principal’s invoice / Proposal.
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;">
+                    5. &nbsp;&nbsp;&nbsp;&nbsp; Write in column “NOT APPLICABLE” (NA), if not asked in tenders Conditions and write ÒNOT QUOTED Ó (NQ) if not quoted by the principal.
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="font-size: 11px;">
+                    6. &nbsp;&nbsp;&nbsp;&nbsp; Each column must be filled in by the local agent.
+                </td>
+            </tr>
         </table>
     </div>
 
-    <br>
 
 
-    <table width="100%">
-        <tr>
-            <td>
-                <span>De...</span>
-            </td>
-
-            <td style="width: 30%">
-            </td>
-
-            <td>
-                <span>APO...</span>
-            </td>
-
-            <
-            <td style="width: 30%">
-            </td>
-
-            <td>
-                <span>PO...</span>
-            </td>
-        </tr>
-    </table>
 </div>
+<footer style="position: fixed; bottom: 0; width: 100%; font-size: 18px; text-align: center">
+    <p>
+        RESTRICTED
+    </p>
+</footer>
 
 </body>
